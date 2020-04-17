d797aa56508588106fef3a593aad05454f9a2623
Issac Wang <lote.mac@icloud.com>
2020-04-17 16:35:18 +0800

    global functions: istable/function/number/table/string

---
+++ Base/global.lua
@@ -0,0 +1,5 @@
+isfunction = function(o) return type(o) == "function" end
+isnumber = function(o) return type(o) == "number" end
+istable = function(o) return type(o) == "table" end
+isstring = function(o) return type(o) == "string" end
+isboolean = function(o) return type(o) == "boolean" end
